25c36d3e014fa8b879c30fb0334e7f909ae82acd
Dmitry Vassiliev <clawfinger@yandex.ru>
2019-01-21 21:51:28 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -146,43 +146,43 @@ private:
 int main(int argc, char* argv[])
 {
 
-//    pid_t pid, sid;
+    pid_t pid, sid;
 
-//    /* Fork off the parent process */
-//    pid = fork();
-//    if (pid < 0) {
-//            exit(EXIT_FAILURE);
-//    }
-//    /* If we got a good PID, then
-//       we can exit the parent process. */
-//    if (pid > 0) {
-//            exit(EXIT_SUCCESS);
-//    }
+    /* Fork off the parent process */
+    pid = fork();
+    if (pid < 0) {
+            exit(EXIT_FAILURE);
+    }
+    /* If we got a good PID, then
+       we can exit the parent process. */
+    if (pid > 0) {
+            exit(EXIT_SUCCESS);
+    }
 
-//    /* Change the file mode mask */
-//    umask(0);
+    /* Change the file mode mask */
+    umask(0);
 
-//    /* Open any logs here */
+    /* Open any logs here */
 
-//    /* Create a new SID for the child process */
-//    sid = setsid();
-//    if (sid < 0) {
-//            /* Log any failure here */
-//            exit(EXIT_FAILURE);
-//    }
+    /* Create a new SID for the child process */
+    sid = setsid();
+    if (sid < 0) {
+            /* Log any failure here */
+            exit(EXIT_FAILURE);
+    }
 
-//    /* Change the current working directory */
-//    if ((chdir("/")) < 0) {
-//            /* Log any failure here */
-//            exit(EXIT_FAILURE);
-//    }
+    /* Change the current working directory */
+    if ((chdir("/")) < 0) {
+            /* Log any failure here */
+            exit(EXIT_FAILURE);
+    }
 
 
-//    /* Close out the standard file descriptors */
-//    close(STDIN_FILENO);
-//    close(STDOUT_FILENO);
-//    close(STDERR_FILENO);
-//    signal(SIGCHLD, SIG_IGN);
+    /* Close out the standard file descriptors */
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+    signal(SIGCHLD, SIG_IGN);
     int port;
     std::string dir;
     std::string host;
